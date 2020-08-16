e715eeec075ba22116e54bfa5717e2e154fc8763
IlyaSteik <ilya.steik@mail.ru>
2020-08-16 14:57:17 +0300

    Updates

---
+++ precache-manifest.7d5035426e544da640daffac893f7b85.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f20b7a362731829fd024d3d258f887c",
+    "url": "/vkminiapp/index.html"
+  },
+  {
+    "revision": "e7d3ae90207325c0411c",
+    "url": "/vkminiapp/static/css/2.19481ff5.chunk.css"
+  },
+  {
+    "revision": "4aa5c85e68b0fee043cd",
+    "url": "/vkminiapp/static/css/main.197e902f.chunk.css"
+  },
+  {
+    "revision": "e7d3ae90207325c0411c",
+    "url": "/vkminiapp/static/js/2.b018ff4d.chunk.js"
+  },
+  {
+    "revision": "4aa5c85e68b0fee043cd",
+    "url": "/vkminiapp/static/js/main.47f933ed.chunk.js"
+  },
+  {
+    "revision": "a79327228b52a953eeb3",
+    "url": "/vkminiapp/static/js/runtime-main.60590f17.js"
+  },
+  {
+    "revision": "9a7e6112b6720174a5bcaf41ce87ca37",
+    "url": "/vkminiapp/static/media/Daikon-SemiBold.9a7e6112.ttf"
+  },
+  {
+    "revision": "3c2fba9ac68f992a1881032adc88e657",
+    "url": "/vkminiapp/static/media/Icon16Chevron.3c2fba9a.svg"
+  },
+  {
+    "revision": "2ecd0e5587da90d5a2cb3d42fde266f7",
+    "url": "/vkminiapp/static/media/Icon34Airplane.2ecd0e55.svg"
+  },
+  {
+    "revision": "97c8c1d59080bd62692f4b7c659dfdb6",
+    "url": "/vkminiapp/static/media/Icon34Box.97c8c1d5.svg"
+  },
+  {
+    "revision": "be442d3fac8e756ae65c131f8e4adc8a",
+    "url": "/vkminiapp/static/media/Icon34Building.be442d3f.svg"
+  },
+  {
+    "revision": "56283d15199236bea84cd6dd5be94ba6",
+    "url": "/vkminiapp/static/media/Icon34Customs.56283d15.svg"
+  },
+  {
+    "revision": "1a440d2ce5cbfbc691b7cc6c94d2ea9c",
+    "url": "/vkminiapp/static/media/Icon34Customs2.1a440d2c.svg"
+  },
+  {
+    "revision": "7220498a182fdd4b04eb0480695e4aeb",
+    "url": "/vkminiapp/static/media/Icon34Error.7220498a.svg"
+  },
+  {
+    "revision": "1500bfbc3283eea31d1953bdc7b76386",
+    "url": "/vkminiapp/static/media/Icon34Inbox.1500bfbc.svg"
+  },
+  {
+    "revision": "42e1a2bf2ad0c53ec6584673cd34c070",
+    "url": "/vkminiapp/static/media/Icon34Like.42e1a2bf.svg"
+  },
+  {
+    "revision": "4d3764cba2abfbb51d2803a4f18f9e9f",
+    "url": "/vkminiapp/static/media/Icon34Message.4d3764cb.svg"
+  },
+  {
+    "revision": "4f334d83a8b779b44c4946cc22a26d95",
+    "url": "/vkminiapp/static/media/Icon34Truck.4f334d83.svg"
+  },
+  {
+    "revision": "de56e6a465e9d9b2ab97c4223ac07b43",
+    "url": "/vkminiapp/static/media/Icon38Message.de56e6a4.svg"
+  },
+  {
+    "revision": "1567b63e057e19447a07a5a63fa56d67",
+    "url": "/vkminiapp/static/media/MainPlaceholderButton.1567b63e.svg"
+  },
+  {
+    "revision": "25fce6f41d5cc8978652646dfd03d378",
+    "url": "/vkminiapp/static/media/MainPlaceholderButtonLoading.25fce6f4.svg"
+  },
+  {
+    "revision": "d22543958135ad048246739b39369334",
+    "url": "/vkminiapp/static/media/MainPlaceholderIcon.d2254395.svg"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/vkminiapp/static/media/SF-Pro-Text-Regular.85bd46c1.ttf"
+  }
+]);
